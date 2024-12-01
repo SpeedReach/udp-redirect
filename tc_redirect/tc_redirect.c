108298565f4128b29357afd4545d9b3a23164340
brian <brian030128@gmail.com>
2024-12-01 19:31:28 +0800

    update

--- tc_redirect/tc_redirect.c
+++ tc_redirect/tc_redirect.c
@@ -85,7 +85,6 @@ int tcdump(struct __sk_buff *ctx) {
 	} 
 	
 	if(header.ip != NULL && bpf_ntohl(header.ip->daddr) == redirect_addr){
-		bpf_printk("wwwww %d %d" , header.ip->id, header.ip->frag_off);
 		u16 id = header.ip->id;
 		if(bpf_map_lookup_elem(&dest_map, &id) == NULL){
 			return TC_ACT_OK;
